25a27ef90d123ff7d5b8b7326425aa99605e0d55
Your Name <you@example.com>
2020-04-05 10:06:30 -0700

    create brief layout for accordion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import Nav from "./components/nav"
+import Accordion from "./components/accordion"
+
 
 function App() {
   return (
+    <>
     <Nav></Nav>
+    <Accordion></Accordion>
+    </>
   );
 }
 
---
+++ src/components/accordion/index.js
@@ -0,0 +1,27 @@
+import React from "react"
+import "./style.scss"
+
+function Accordion() {
+    return (
+        <div className="accordion">
+            <div className="item">
+                <div className="gold"></div>
+                <img src="https://images.unsplash.com/photo-1580828343064-fde4fc206bc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80" />
+            </div>
+            <div className="item">
+                <div className="silver"></div>
+                <img src="https://images.pexels.com/photos/673648/pexels-photo-673648.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+            </div>
+            <div className="item">
+                <div className="gray"></div>
+                <img src="https://p68.f3.n0.cdn.getcloudapp.com/items/GGuNDoJk/Image%202020-04-04%20at%203.36.25%20PM.png?v=3638a30d759778773bc73717b54df1de" />
+            </div>
+            <div className="item">
+                <div className="bronze"></div>
+                <img src="https://images.pexels.com/photos/165228/pexels-photo-165228.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/>
+            </div>
+        </div>
+    )
+}
+
+export default Accordion;
